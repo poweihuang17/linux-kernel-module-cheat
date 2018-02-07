bd4015a68cfad7989a7954e4222185594cc48e32
Po-wei Huang <poweihuang17@gmail.com>
2018-02-07 11:38:30 +0800

    Finish atomic_add experiment.

--- kernel_module/atomic_add.c
+++ kernel_module/atomic_add.c
@@ -11,10 +11,11 @@ atomic_t my_atomic;
 int __init atomic_add_init(void){
 	int i;
 	atomic_set( &my_atomic, 5);
-	printk("After atomic_set, my atomic counter is %d\n.", atomic_read(my_atomic));
+	printk("After atomic_set, my atomic counter is %d\n.", atomic_read(&my_atomic));
 	i=3;
 	atomic_add(i, &my_atomic);
-	printk("After atomic_add, my atomic counter is %d\n.", atomic_read(my_atomic));
+	printk("After atomic_add, my atomic counter is %d\n.", atomic_read(&my_atomic));
+	return 0;
 }
 
 void __exit atomic_add_exit(void){
