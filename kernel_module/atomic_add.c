ddefc095ebf2d0a367d905e9caefb3df8eceaac4
Po-wei Huang <poweihuang17@gmail.com>
2018-02-07 11:25:47 +0800

    Start to work on atomic operation api.

---
+++ kernel_module/atomic_add.c
@@ -0,0 +1,25 @@
+#include <asm/atomic.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
+MODULE_LICENSE("GPL");
+static int __init atomic_add_init(void);
+static void __exit atomic_add_exit(void);
+
+atomic_t my_atomic;
+
+int __init atomic_add_init(void){
+	int i;
+	atomic_set( &my_atomic, 5);
+	printk("After atomic_set, my atomic counter is %d\n.", atomic_read(my_atomic));
+	i=3;
+	atomic_add(i, &my_atomic);
+	printk("After atomic_add, my atomic counter is %d\n.", atomic_read(my_atomic));
+}
+
+void __exit atomic_add_exit(void){
+	printk("exit!\n");
+}
+
+module_init(atomic_add_init);
+module_exit(atomic_add_exit);
